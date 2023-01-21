e2fc40bcb7475adf4e46b97ac359cae8de30ee9b
rasoul6094 <123249130+rasoul6094@users.noreply.github.com>
2023-01-21 17:48:56 +0330

    create tow-way linked list

--- typinggame.c
+++ typinggame.c
@@ -6,6 +6,14 @@
 #include<dos.h>
 
 
+
+struct node{  
+    char  *data;  
+    struct node *previous;  
+    struct node *next;  
+};   
+
+
 DWORD WINAPI keypress_callback( LPVOID args ){
   void (*callback)(char) = (void (*)(char))((void *)(args));
 
@@ -53,7 +61,35 @@ void setcolor(int ForgC) //function to change setcolor
  }
 
 
-
+//Represent the head and tail of the doubly linked list  
+struct node *head, *tail = NULL;  
+   
+//addNode() will add a node to the list  
+void addNode(char *data) {  
+    //Create a new node  
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));  
+    newNode->data = data;  
+      
+    //If list is empty  
+    if(head == NULL) {  
+        //Both head and tail will point to newNode  
+        head = tail = newNode;  
+        //head's previous will point to NULL  
+        head->previous = NULL;  
+        //tail's next will point to NULL, as it is the last node of the list  
+        tail->next = NULL;  
+    }  
+    else {  
+        //newNode will be added after tail such that tail's next will point to newNode  
+        tail->next = newNode;  
+        //newNode's previous will point to tail  
+        newNode->previous = tail;  
+        //newNode will become new tail  
+        tail = newNode;  
+        //As it is last node, tail's next will point to NULL  
+        tail->next = NULL;  
+    }  
+}  
 
 
 
